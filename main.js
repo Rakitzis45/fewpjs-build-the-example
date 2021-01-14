3731523e87dab7f57fcdafa2259e64283a93bd12
Rakitzis45 <michael.rakitzis@gmail.com>
2021-01-14 17:12:41 -0500

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likeGlyph = document.querySelector(".like-glyph")
+let post = document.querySelector(".media-post")
+let body = document.querySelector("body")
+
+document.addEventListener("DOMContentLoaded",
+  console.log("i loaded"))
+
+body.addEventListener('click', turnRed)
+function turnRed(e) {
+  mimicServerCall("url")
+  .then(fakeJson)
+  .catch(error)
+
+  if (e.target.className === "like-glyph" || e.target.className === "activated-heart"){
+    if (e.target.className === "activated-heart"){
+      e.target.innerHTML = EMPTY_HEART;
+      e.target.className = "like-glyph";
+    } else{
+        e.target.innerHTML = FULL_HEART;
+        e.target.className = "activated-heart";
+    }
+  }
+}
+function fakeJson(arg){
+  console.log(arg)
+  return arg
+}
+function error(error) {
+  document.getElementById("modal").className= "";
+  setTimeout(removeError, 5000);
+  console.log("error")
+}
+function removeError(){
+  document.getElementById("modal").className= "hidden"
+}
+
 
 
 
